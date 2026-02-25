1598c85613949f1e92f91317baa3b6bcd57f8f28
Jacob Ras <jacobras.nl@gmail.com>
2026-02-25 11:34:27 +0100

    Remove leftover mermaid rendering

--- cli/src/main/kotlin/nl/jacobras/codebaseobserver/cli/command/measure/gradle/MeasureGradleCommand.kt
+++ cli/src/main/kotlin/nl/jacobras/codebaseobserver/cli/command/measure/gradle/MeasureGradleCommand.kt
@@ -134,37 +134,12 @@ class MeasureGradleCommand internal constructor(
                     dependencies[module] = deps
                 }
             }
-
-            println("MERMAID start")
-            println(drawMermaid(modules, dependencies))
-            println("MERMAID end")
-
             // Calculate the height of the dependency graph using topological sort
             calculateGraphHeight(modules, dependencies)
         } catch (e: Exception) {
             println("Failed to calculate module height because of ${e.message}")
             Pair(0, emptyList())
         }
-    }
-
-    private fun drawMermaid(
-        modules: List<String>,
-        dependencies: Map<String, List<String>>
-    ): String {
-        val builder = StringBuilder()
-        builder.appendLine("graph TD\n") // Top-down layout
-
-        modules.forEach { module ->
-            builder.appendLine("    $module")
-        }
-
-        builder.appendLine("\n%% Dependencies")
-        dependencies.forEach { (module, deps) ->
-            deps.forEach { dep ->
-                builder.appendLine("    $module --> $dep")
-            }
-        }
-        return builder.toString()
     }
 
     private fun calculateGraphHeight(
