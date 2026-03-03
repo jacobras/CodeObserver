4198940075fc9b19c779579f49d4c09713fba2d0
Jacob Ras <jacobras.nl@gmail.com>
2026-03-03 21:59:10 +0100

    Add test for `GraphUtil`

---
+++ server/src/test/kotlin/nl/jacobras/codebaseobserver/server/graph/GraphUtilTest.kt
@@ -0,0 +1,35 @@
+package nl.jacobras.codebaseobserver.server.graph
+
+import assertk.assertThat
+import assertk.assertions.isEqualTo
+import kotlin.test.Test
+
+class GraphUtilTest {
+
+    @Test
+    fun `calculate betweenness centrality score`() {
+        val modules = mapOf(
+            "app" to listOf("feature1", "feature2", "feature3", "domain"),
+            "feature1" to listOf("feature2", "domain"),
+            "feature2" to listOf("domain"),
+            "feature3" to listOf("feature2", "domain"),
+            "domain" to listOf("util:text"),
+            "util:text" to listOf("util"),
+            "util" to emptyList()
+        )
+
+        val scores = GraphUtil.calculateBetweennessCentralityScore(modules)
+
+        assertThat(scores).isEqualTo(
+            mapOf(
+                "app" to 0.0,
+                "domain" to 8.0,
+                "feature1" to 0.0,
+                "feature2" to 0.0,
+                "feature3" to 0.0,
+                "util" to 0.0,
+                "util:text" to 5.0
+            )
+        )
+    }
+}
